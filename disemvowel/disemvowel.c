7f99fdc31118d98f61e08a5b0e3185d0cadfd511
Cookie Vang <vang2660@morris.umn.edu>
2020-09-17 02:53:42 -0500

    Explore recursive solution

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -6,36 +6,43 @@
 //in method, track the count of the number of vowels
 //create new string with appropriate length (without vowels and null terminator)
 
-char removeVowel(char * str, int deleteIndex){ //removes one vowel at a given index
-  char *alterString = memmove(&str[deleteIndex], &str[deleteIndex + 1], strlen(str) - deleteIndex); //memmove handles removing a character at an index in the array
-  return alterString //returns the string without the vowels
-}
-
-int getIndex(char *str){ //return the index of a vowel in a string
-  int index;  
-  for (int i = 0; i < strlen(); ++i){
-  	if (str[i] = "a" || "A" || "e" || "E" || "i" || "I" || "o" || "O" || "u" || "U"){
-		index = i;
-		break;
-	}
-}
-
-
-int vowelCount(char *str){ //counts the number of vowels, will help determine length of new string
+int vowelCount(char *str){ //counts the number of vowels in a string
   int vowelCounter;
-  for (int i = 0; i < strlen(); ++i){
+  for (int i = 0; i < strlen(str); ++i){
 	if (str[i] = "a" || "A" || "e" || "E" || "i" || "I" || "o" || "O" || "u" || "U"){
 		vowelCounter++;
-	}
+  	}
   }
-  return vowelCounter;
+  return vowelCounter
 }
+
+char removeVowels(char * str){ //uses recursion to remove the vowels in a string
+  int index;
+  int vowelNum;
+  char *newString;
+  vowelNum = vowelCount(str);
+  if (vowelNum == 0) { //if the string has no vowels, then return the string which should have no vowels
+	return str;
+  }
+  else { //if there are vowels, loop through the string and remove them, then call removeVowels again
+	for (int i = 0; i < strlen(str); ++i){ 
+		if (str[i] = "a" || "A" || "e" || "E" || "i" || "I" || "o" || "O" || "u" || "U"){
+			index = i;
+			str = memmove(&str[index], &str[index + 1], strlen(str) - index);
+			removeVowels(str);
+		}
+	}
+  }	
+}
+
 
 char *disemvowel(char *str) {
   int strLength = strlen(str); //length of input string
   int vowelNum = vowelCount(str); //how many vowels there are
   int newLength = strLength - vowelNum; //length the of input string without vowels
-
+  char *result; 
+  result = (char*) calloc(newLength+1, sizeof(char)); // creating a new string
+  removeVowels(str);
   //use removeVowel and getIndex functions here
 
   result = (char*) calloc(newLength+1, sizeof(char)); //new string with the null terminator
