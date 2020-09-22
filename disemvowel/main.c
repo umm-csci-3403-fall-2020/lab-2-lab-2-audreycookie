a36a93be14923d29562d8bcb2fd801eff4625bc3
Cookie Vang <vang2660@morris.umn.edu>
2020-09-22 12:47:13 -0500

    Try to fix leak in main

--- disemvowel/main.c
+++ disemvowel/main.c
@@ -11,7 +11,9 @@ int main(int argc, char *argv[]) {
   line = (char*) malloc (size + 1);
 
   while (getline(&line, &size, stdin) > 0) {
-    printf("The result of using disemvowel on the input string: %s\n", disemvowel(line));
+    char*  result = disemvowel(line);
+    printf("The result of using disemvowel on the input string: %s\n", result);
+    free(result);
   }
   free(line);
 }
